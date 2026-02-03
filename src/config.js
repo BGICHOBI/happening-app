e05c8c7311d91c8b509006c512c117f23ff21b9a
bgichobi <kabingabrian@gmail.com>
2026-02-03 09:48:29 +0300

    Ensure production API URL is used

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://happening-production.up.railway.app';
 
 //'http://localhost:5000';
 //'https://happening-production.up.railway.app';
